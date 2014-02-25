9decff01ea6fdc512a85df019030299795d65b68
Robin Hack <rhack@redhat.com>
2014-02-25 13:26:26 +0100

    example1: Get OS page size via sysconf.

--- example1.c
+++ example1.c
@@ -2,19 +2,21 @@
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static const int x = 10;
 
 int main ()
 {
-    unsigned long addr = (unsigned long) &x & ~(4096 - 1);
+    const long page_size = sysconf(_SC_PAGESIZE);
 
+    unsigned long addr = (unsigned long) &x & ~(page_size - 1);
     printf ("%original value of x: %d\n", x);
     printf ("address of x: %p\n", &x);
     printf ("page rounded address of x: 0x%x\n", addr);
 
     int *n_x = &x;
-    const int ret = mprotect ((void *)addr, 4096, PROT_WRITE | PROT_READ | PROT_EXEC);
+    const int ret = mprotect ((void *)addr, page_size, PROT_WRITE | PROT_READ | PROT_EXEC);
     if (ret == -1) {
         perror ("mprotect!");
         return EXIT_FAILURE;
