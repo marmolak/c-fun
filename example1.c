401559a32e92afbbc077ccd514e0796d545453e1
Robin Hack <rhack@redhat.com>
2014-02-25 12:09:39 +0100

    Add first example.

---
+++ example1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static const int x = 10;
+
+int main ()
+{
+    unsigned long addr = (unsigned long) &x & ~(4096 - 1);
+
+    printf ("%original value of x: %d\n", x);
+    printf ("address of x: %p\n", &x);
+    printf ("page rounded address of x: 0x%x\n", addr);
+
+    int *n_x = &x;
+    const int ret = mprotect ((void *)addr, 4096, PROT_WRITE | PROT_READ | PROT_EXEC);
+    if (ret == -1) {
+        perror ("mprotect!");
+        return EXIT_FAILURE;
+    }
+
+    /* write new value to x */
+    *n_x = 666;
+
+    printf ("n_x address: %p\n", n_x);
+    printf ("new value of x: %d\n", x);
+    printf ("new value of n_x: %d\n", *n_x);
+
+    return EXIT_SUCCESS;
+}
