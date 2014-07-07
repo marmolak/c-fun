f5382c885e78bf8f6184dbb80ede4b278ac3aff9
Robin Hack <rhack@redhat.com>
2014-07-07 13:27:25 +0200

    Added new exambple. Int overflow and undefined C behaviour.

---
+++ example3.c
@@ -0,0 +1,16 @@
+int main ()
+{
+    int id = 1;
+    int p;
+    for (p = 0; p < 32; ++p) {
+        id <<= 1;
+    }
+
+    printf ("%d\n", id);
+
+    id = 1;
+    id <<= 32;
+    printf ("%d\n", id);
+
+    printf ("%d\n", 1<<32);
+}
